bc458eb4ce287cc720522f159cc3e6fa809b1d7d
Tom Jarle Christiansen <fuzzbin@gmail.com>
2023-06-15 22:00:37 +0200

    Clean up

--- Veilys-datalogger/datalogger.py
+++ Veilys-datalogger/datalogger.py
@@ -1,4 +1,3 @@
-
 # Python script som plotter veilysdata fra databasen
 # Lisens: CC BY-SA 4.0 - VTFK - 2023
 
@@ -8,7 +7,6 @@ import matplotlib.dates as mdates
 import datetime
 import os
 from dotenv import load_dotenv
-
 
 load_dotenv()
 
@@ -25,16 +23,18 @@ myresult = mycursor.fetchall()
 
 tid = []
 lys = []
+manuell = []
 dt = []
 
 for x in myresult:
   tid.append(x[1])
   lys.append(x[3])
+  manuell.append(x[4])
   dt.append(datetime.datetime.fromtimestamp(x[1] - 3600*2))
-
 
 plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%d.%m.%Y'))
 plt.gca().xaxis.set_major_locator(mdates.DayLocator())
 plt.plot(dt, lys)
+plt.plot(dt, manuell)
 plt.gcf().autofmt_xdate()
 plt.show()
