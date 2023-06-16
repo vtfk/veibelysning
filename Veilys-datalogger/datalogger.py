6dfffada60555c6522ebf6ee0785c4735b0c4401
Tom Jarle Christiansen <fuzzbin@gmail.com>
2023-06-16 08:22:50 +0200

    Added comments and fixed axis on plot

--- Veilys-datalogger/datalogger.py
+++ Veilys-datalogger/datalogger.py
@@ -1,6 +1,7 @@
 # Python script som plotter veilysdata fra databasen
 # Lisens: CC BY-SA 4.0 - VTFK - 2023
 
+# Importerer nødvendige biblioteker
 import mysql.connector
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
@@ -8,8 +9,10 @@ import datetime
 import os
 from dotenv import load_dotenv
 
+# Henter ut miljøvariabler fra .env-fil
 load_dotenv()
 
+# Kobler til databasen
 mydb = mysql.connector.connect(
   host = os.getenv("HOST"),
   user = os.getenv("USER"),
@@ -17,21 +20,29 @@ mydb = mysql.connector.connect(
   database = os.getenv("DATABASE")
 )
 
+# Henter ut data fra databasen
 mycursor = mydb.cursor()
 mycursor.execute("SELECT * FROM veilys")
 myresult = mycursor.fetchall()
 
+# Lager lister for å lagre data fra databasen
 tid = []
 lys = []
 manuell = []
 dt = []
 
+# Henter ut data fra databasen og klargjør til plotting
 for x in myresult:
   tid.append(x[1])
   lys.append(x[3])
   manuell.append(x[4])
   dt.append(datetime.datetime.fromtimestamp(x[1] - 3600*2))
 
+# Plotter veilysdata fra databasen
+plt.title('Veilys')
+plt.xlabel("Tidspunkt")
+plt.ylabel("Status lys")
+plt.yticks([])
 plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%H:%M -  %d.%m.%Y'))
 plt.gca().xaxis.set_major_locator(mdates.HourLocator(interval=2))
 plt.plot(dt, lys)
